a81eaf17c6da9e6db3269e212ba47f1eca8cd628
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-11 12:20:28 -0500

    completed with expla.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,31 +1,20 @@
 import ipdb
 class Anagram:
     def __init__(self, word):
-        self.sorted_letter = sorted([letter for letter in word])
-    
-    def match(self, words_list):
+        self.word = sorted(word)
+
+    def match(self, input_list):
         match_list = []
-        for word in words_list:
-            if sorted([letter for letter in word]) == self.sorted_letter:
+        for word in input_list:
+            sorted_word = sorted(word)
+            if sorted_word== self.word:
                 match_list.append(word)
         return match_list
+    # 인풋 리스트에 단어가 3개라면, 포룹이 시작되고 첫번째 단어가 소트되고 그 단어가 이닛에서 받은 소트된 단어와 같은지 첵하고
+    # 동일하면  매치리스트 리스트에 넣음. 나머지 2개 단어도 반복함. if 문이 포룹 안에 잇어야함. 밖에 있으면 포룹이 3번 다 돌고
+    #이프문이 실행되기때문에 각 단어를 체크하지 않음. 3번 다 실행하고 끝나면 리턴해야함. 그래서 리턴문은 밖에 있음.
 
+        
 
-
-    
-
-
-
-
-
-
-
-# Your class, Anagram should take a word on initialization; instances should respond
-#  to a match() instance method that takes a list of possible anagrams. It should 
-#  return all matches in a list. If no matches exist, it should return an empty list.
-
-# In other words, given: 'listen' and ['enlists', 'google', 'inlets', 'banana'] the program should return ['inlets'].
-
-# listen = Anagram("listen")
-# listen.match(['enlists', 'google', 'inlets', 'banana'])
-# # => ['inlets']
+hello = Anagram('hello')
+print(hello.match(['llohe', 'hi']))
